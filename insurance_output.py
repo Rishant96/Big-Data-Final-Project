9cf240afb2f7257b9f9096725a1b1414d5d4e52a
Rishant96 <rishant.dutt96@gmail.com>
2019-11-25 18:37:48 -0500

    insurance_output fixed

---
+++ insurance_output.py
@@ -0,0 +1,25 @@
+import glob
+
+def get_output(predictions, output_file, input_folder_name='testing_portfolios'):
+    path = input_folder_name # use your path
+    all_files = glob.glob(path + "/*.csv")
+    files = []
+    for f in all_files:
+        t = f[24:]
+        t = t[:-4]
+        files.append(t)
+    def sort_file(item):
+        return int(item.split('_')[1])
+    files = sorted(files, key=sort_file)
+    assert len(predictions) == len(files)
+    output = []
+    for i in range(len(predictions)):
+        pred = predictions[i]
+        output.append((files[i], pred))
+
+        if output_file is not None:
+            op_pd = pd.DataFrame(output, columns=['ID','ln_LR'])
+            op_pd.to_csv(output_file + '.csv', index=False)
+            return op_pd
+        else:
+            return output
